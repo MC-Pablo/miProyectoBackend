542df5f1e97404d63064e8ed26fdff5ebdd8bf65
Pablo Conturso <pablomconturso@gmail.com>
2024-06-25 21:22:29 -0300

    wip

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -1,22 +1,50 @@
-
 const socket = io();
-
+const productList = document.getElementById("productList");
 
 socket.on("connect", () => {
-    console.log("Conectado al Server");
+  console.log("Conectado al Server");
 });
-
 
 socket.on("saludo-respuesta", (data) => {
-    console.log(data.message);
+  console.log(data.message);
 });
 
-socket.on("realTimeProducts", (realtimeproducts) =>{
-    console.log (realtimeproducts);
+socket.on("realTimeProducts", (realtimeproducts) => {
+  realtimeproducts.forEach((product) => {
+    const listItem = document.createElement("li");
 
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.name = "selectedProducts";
+    checkbox.value = product.id;
+    listItem.appendChild(checkbox);
+
+    const productInfo = document.createElement("span");
+    productInfo.textContent = `${product.title} - Precio: ${product.price}`;
+    listItem.appendChild(productInfo);
+
+    productList.appendChild(listItem);
+  });
+
+  const productForm = document.getElementById("productForm");
+  const addToCartButton = document.getElementById("addToCartButton");
+  let selectedProducts = [];
+  productForm.addEventListener("change", (event) => {
+    if (
+      event.target.type === "checkbox" &&
+      event.target.name === "selectedProducts"
+    ) {
+      let selectedProducts = Array.from(
+        productForm.querySelectorAll('input[type="checkbox"]:checked')
+      ).map((checkbox) => checkbox.value);
+    }
+  });
+
+  addToCartButton.addEventListener("click", () => {
+    console.log("Productos agregados al carrito:", selectedProducts);
+  });
 });
-
 
 socket.on("disconnect", () => {
-    console.log("Se desconecto el server");
+  console.log("Se desconecto el server");
 });
